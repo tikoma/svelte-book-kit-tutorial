c469271f6fa8d80188b985fbee44aa0d5e62a02c
生駒孝夫 <ikoma@pious.local>
2023-07-03 16:27:54 +0900

    missplelled mongodb: corrected

--- src/lib/server/mongodb.js
+++ src/lib/server/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from  'mongodb';
 import { env } from '$env/dynamic/private';
 
-export const client = new MongoClient( env.MONGODB_URI ?? 'monbodb://dumy' );
+export const client = new MongoClient( env.MONGODB_URI ?? 'mongodb://dumy' );
 export const database = client.db();
 
